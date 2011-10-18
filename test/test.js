bd566f79ee384927dd1bf69318461b2862f989ba
Pau Fernández <pau.fernandez@upc.edu>
2011-10-18 23:05:50 +0200

    Añado un script para testear los programas

---
+++ test/test.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+var fs = require("fs");
+var childProcess = require("child_process");
+
+var FILE = process.argv[2];
+
+function returnStdOut(_callback) {
+   return function (error, stdout, stderr) {
+      _callback(stdout);
+   }
+}
+
+function realOutput(_callback) {
+   childProcess.exec("g++ " + FILE, function (error, stdout, stderr) {
+      childProcess.exec("./a.out", returnStdOut(_callback));
+   });
+}
+
+function interpretedOutput(_callback) {
+   childProcess.exec("./cc.js " + FILE, returnStdOut(_callback));
+}
+
+realOutput(function (rOutput) {
+   interpretedOutput(function (iOutput) {
+      if (rOutput == iOutput) {
+         console.log('PASS');
+      } else {
+         console.log('FAIL');
+      }
+   });
+});
