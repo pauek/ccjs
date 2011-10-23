0bd27d8171dad8c88c178abdac592045394d4b5f
Pau Fernández <pau.fernandez@upc.edu>
2011-10-23 23:49:33 +0200

    Faltaba añadir estos

---
+++ tests/array5.cc
@@ -0,0 +1,7 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   int T1[] = { 1, 2, 3, 4, 5 };
+}
---
+++ tests/class1.cc
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+   int a, b;
+};
+
+int main() {
+   A x;
+}
